59eb4892965b21303643e30654c973e5cfe9cd2c
matkatmusic <matkatmusic@gmail.com>
2020-05-15 15:25:34 -0700

    added Part2 instructions

--- main.cpp
+++ main.cpp
@@ -1,76 +1,66 @@
 /*
-Project 5: Part 1 / 4
- video Chapter 2 - Part 12
+Project 5: Part 2 / 4
+ video: Chapter 3 Part 1
 
- Create a branch named Part1
+Create a branch named Part2
 
-Purpose:  This project continues developing Project3 and will take you through how to write code that doesn't leak as well as how to refactor. 
-
- Destructors
-
- If you didn't do Project3, write 3 UDTs below (where it asks for Copied UDTs) that have: 
-        member functions,
-        member variables, 
-        constructors that initialize the members. 
-        for() or while() loops that modify member variables
-        
- Define your implementations of all functions OUTSIDE of the class. 
- Then do instruction 2)
+ The 'this' keyword
  
- If you DID do Project 3, do instruction 1)
+ The purpose of this project part is to show you how accessing member variables of objects INSIDE member functions is very similar to accessing member variables of objects OUTSIDE of member functions, via 'this->' and via the '.' operator.
+ This project part will break the D.R.Y. rule, but that is fine for the purpose of this project part.
  
- 1) Copy a few of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
- 2) add destructors
-        make the destructors do something like print out the name of the class.
-
-	If you already did Chapter2 part 10 on Coliru and don't have a Project3, just grab some UDTs from your Coliru Ch2P10 submission. 
  
- 3) add 2 new UDTs that use only the types you copied above.
- 4) Add destructors to these 2 new types that do stuff.  
-        maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
+ 1) if you don't have any std::cout statements in main() that access member variables of your U.D.Ts
+         write some.
+    You can copy some from your Project3's main() if needed.
  
- 5) copy over your main() from the end of Project3 and get it to compile with the types you copied over.
-        remove any code in main() that uses types you didn't copy over.
+ 2) For each std::cout statement in main() that accessed member variables of your types or printed out the results of your member function calls,
+        a) write a member function that prints the same thing out, but uses the proper techniques inside the member functions to access the same member variables/functions.
+        b) be explicit with your use of 'this->' in those member functions so we see how you're accessing/calling those member variables and functions *inside*
+        c) call that member function after your std::cout statement in main.
+        d) you should see 2 (almost) identical messages in the program output for each member function you add:
+            one for the std::cout line, and one for the member function's output
  
- 6) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
  
- 7) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
- 
+ 3) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  */
 
 /*
- copied UDT 1:
+ example:
  */
-
-/*
- copied UDT 2:
- */
-
-/*
- copied UDT 3:
- */
-
-/*
- new UDT 4:
- */
-
-/*
- new UDT 5:
- */
-
-/*
- MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
-
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
-    Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
-
- send me a DM to review your pull request when the project is ready for review.
-
- Wait for my code review.
- */
+#include <iostream>
+namespace Example
+{
+    //a User-Defined Type
+    struct MyFoo
+    {
+        MyFoo() { std::cout << "creating MyFoo" << std::endl; }
+        ~MyFoo() { std::cout << "destroying MyFoo" << std::endl; }
+		
+		// 2a) the member function whose function body is almost identical to the std::cout statement in main.
+        void memberFunc() 
+		{ 
+            // 2b) explicitly using 'this' inside this member function.
+			std::cout << "MyFoo returnValue(): " << this->returnValue() << " and MyFoo memberVariable: " << this->memberVariable << std::endl; 
+		}  
+		
+        int returnValue() { return 3; }
+        float memberVariable = 3.14f;
+    };
+    
+    int main()
+    {
+        //an instance of the User-Defined Type named mf
+        MyFoo mf;
+		
+        // 1) a std::cout statement that uses mf's member variables
+        std::cout << "mf returnValue(): " << mf.returnValue() << " and mf memberVariable: " << mf.memberVariable << std::endl; 
+		
+        // 2c) calling mf's member function.  the member function's body is almost identical to the cout statement above.
+        mf.memberFunc();
+        return 0;
+    }
+}
 
 #include <iostream>
 #include <vector>
@@ -82,10 +72,8 @@ Purpose:  This project continues developing Project3 and will take you through h
 #define MOTORBIKE_ON_MOTORWAY_COEFF 0.0000006
 #define LORRY_ON_MOTORWAY_COEFF 0.0000006
 
-class Car
+struct Car
 {
-public:
-    
     Car(std::string name_, double speed_, int passengers_) : name(name_), speed(speed_), passengers(passengers_) 
     {
         for(size_t i = 0; i < passengers; ++i)
@@ -94,25 +82,27 @@ public:
         }
         std::cout << name << " was created" << std::endl;
     }
+	
     ~Car()
     {
         std::cout << name << " was destroyed" << std::endl;
     }
+	
     double getSpeed();
     void calculateAndPrintRisk();
-    
 private:
-    std::string name{""};
+    std::string name;
     double speed{0};
     int passengers;
     std::vector<double> passengerRiskArray;
     std::vector<double> riskCoeffArray = {0.5, 0.7, 0.8, 0.8, 0.9};
-    
 };
+
 double Car::getSpeed()
 {
     return speed;
 }
+
 void Car::calculateAndPrintRisk()
 {
     for(size_t i = 0; i < passengerRiskArray.size(); ++i)
@@ -121,10 +111,9 @@ void Car::calculateAndPrintRisk()
         std::cout << "Risk to passenger " << i + 1 << " is: " << passengerRiskArray[i] << " measured in fatalities per journey"<< std::endl;
     }
 }
-class Motorbike
+
+struct Motorbike
 {
-public:
-    
     Motorbike(std::string name_, double speed_, int passengers_) : name(name_), speed(speed_), passengers(passengers_)
     {
         for(size_t i = 0; i < passengers; ++i)
@@ -133,26 +122,27 @@ public:
         }
         std::cout << name << " was created" << std::endl;
     }
+	
     ~Motorbike()
     {
         std::cout << name << " was destroyed" << std::endl;
     }
+	
     double getSpeed();
     void calculateAndPrintRisk();
-    
-    
 private:
-    std::string name{""};
+    std::string name;
     double speed{0};
     int passengers;
     std::vector<double> passengerRiskArray;
     std::vector<double> riskCoeffArray = { 1.85, 1.95};
-    
 };
+
 double Motorbike::getSpeed()
 {
     return speed;
 }
+
 void Motorbike::calculateAndPrintRisk()
 {
     for(size_t i = 0; i < passengerRiskArray.size(); ++i)
@@ -161,10 +151,9 @@ void Motorbike::calculateAndPrintRisk()
         std::cout << "Risk to passenger " << i + 1 << " is: " << passengerRiskArray[i] << " measured in fatalities per journey"<< std::endl;
     }
 }
-class Lorry
+
+struct Lorry
 {
-public:
-    
     Lorry(std::string name_, double speed_, int passengers_) : name(name_), speed(speed_), passengers(passengers_)
     {
         for(size_t i = 0; i < passengers; ++i)
@@ -173,25 +162,27 @@ public:
         }
         std::cout << name << " was created" << std::endl;
     }
+	
     ~Lorry()
     {
         std::cout << name << " was destroyed" << std::endl;
     }
+	
     double getSpeed();
     void calculateAndPrintRisk();
-
 private:
-    std::string name{""};
+    std::string name;
     double speed{0};
     int passengers;
     std::vector<double> passengerRiskArray;
     std::vector<double> riskCoeffArray = { 0.2, 0.2, 0.2};
-    
 };
+
 double Lorry::getSpeed()
 {
     return speed;
 }
+
 void Lorry::calculateAndPrintRisk()
 {
     for(size_t i = 0; i < passengerRiskArray.size(); ++i)
@@ -200,13 +191,11 @@ void Lorry::calculateAndPrintRisk()
         std::cout << "Risk to passenger " << i + 1 << " is: " << passengerRiskArray[i] << " measured in fatalities per journey"<< std::endl;
     }
 }
-class Motorway
-{
-public:
 
+struct Motorway
+{
     Motorway()
     {
-
         std::cout << "MotorWay created" << std::endl;
     }
     ~Motorway()
@@ -217,27 +206,28 @@ public:
     void addMotorBike(Motorbike* motorbike);
     void addLorry(Lorry* lorry);
     void calculateAndPrintChanceOfAccident();
-    
 private:
-
     std::vector<Car> carsArray;
     std::vector<Motorbike> motorbikesArray;
     std::vector<Lorry> lorriesArray;
     double chanceOfAccident;
-    
 };
+
 void Motorway::addCar(Car* car)
 {
     carsArray.push_back(*car);
 }
+
 void Motorway::addMotorBike(Motorbike* motorbike)
 {
     motorbikesArray.push_back(*motorbike);
 }
+
 void Motorway::addLorry(Lorry* lorry)
 {
     lorriesArray.push_back(*lorry);
 }
+
 void Motorway::calculateAndPrintChanceOfAccident()
 {
     double sumSpeedCars = 0;
@@ -266,7 +256,25 @@ void Motorway::calculateAndPrintChanceOfAccident()
                         avgSpeedLorries * LORRY_ON_MOTORWAY_COEFF;
             std::cout << "Chance of an accident on this stretch of motorway is  " << chanceOfAccident << " measured in accidents per hour" << std::endl;
 }
-int main() {
+
+/*
+ MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
+
+ Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ If you didn't already: 
+    Make a pull request after you make your first commit
+    pin the pull request link and this repl.it link to our DM thread in a single message.
+
+ send me a DM to review your pull request when the project is ready for review.
+
+ Wait for my code review.
+ */
+
+int main() 
+{
+	Example::main();
+	
     Car ford("ford", 70,3);
     Motorbike harley("harley", 80,2);
     Motorbike ducati("ducati", 110,1);
